bd0d403ab40650e6ea8d8e04d1d72748d9fd91ed
evanmiller29 <evanmiller29@gmail.com>
2016-11-25 13:54:26 +1300

    Re-factoring
    Moving feature_engineering into one cohesive approach

--- feature_engineering.py
+++ feature_engineering.py
@@ -14,41 +14,37 @@ def data_formatting(train, test, numeric_vars, log):
     import pandas as pd
     import numpy as np
     
+    train['data'] = 'train'
+    test['data'] = 'test'
+    
+    ttl_df = pd.concat([train, test], axis = 0)
+    
     print('Formatting data..')
     
-    #### Want to first combine the two datasets and do the feature engineering there. 
-    #### Then once you're done split them back into test and train sets
-    
-    ttl_df = pd.concat([train, test], 1)
-    
-    train_numeric = train[numeric_vars]   
-    test_numeric = test[numeric_vars]
+    ttl_numeric = ttl_df[numeric_vars]
     
     if log:
-        train_numeric == np.log1p(train_numeric)
-        test_numeric == np.log1p(test_numeric)
+        ttl_numeric = np.log1p(ttl_numeric)
         print('Taking the log of numeric variables')
         
     train_cat = train.drop(numeric_vars, 1)
     test_cat = test.drop(numeric_vars, 1)
+    ttl_cat = ttl_df.drop(numeric_vars, 1)
+    
+    data = ttl_cat['data']
+    ttl_cat = ttl_cat.drop('data', 1)       
+    ttl_cat = pd.get_dummies(ttl_cat)
 
-    train_cat = pd.get_dummies(train_cat)
-    test_cat = pd.get_dummies(test_cat)
-
-    X_train = pd.concat([train_numeric, train_cat], axis = 1)
-    X_test = pd.concat([test_numeric, test_cat], axis = 1)
-
-    train_cols = X_train.columns.values
-    test_cols = X_test.columns.values
-
-    diffs = list(set(train_cols) - set(test_cols))
-
-    for col in diffs:
-        X_test[col] = 0
-         
+    X_ttl = pd.concat([ttl_numeric, ttl_cat, data], axis = 1)
+    X_train = X_ttl.loc[X_ttl.data == 'train', :]
+    X_test = X_ttl.loc[X_ttl.data == 'test', :]
+    
+    X_train = X_train.drop('data', 1)
+    X_test = X_test.drop('data', 1)
+             
     X_train = X_train.fillna(X_train.mean())
     X_test = X_test.fillna(X_test.mean())
-
+       
     X_train = X_train.as_matrix()
     X_test = X_test.as_matrix()
     
